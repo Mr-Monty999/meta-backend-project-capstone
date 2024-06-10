9b21b9126e0564ba36c3cb9e1be366f50831bc7d
Mr-Monty999 <montser.mohmed2022@gmail.com>
2024-06-10 23:28:12 +0300

    add booking and menu items apis

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from .serializers import MenuItemSerializer,BookingSerializer
+from .models import Menu,Booking
 
 # Create your views here.
 
@@ -6,3 +11,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class  BookingViewSet(viewsets.ModelViewSet):
+   queryset =  Booking.objects.all()
+   serializer_class =  BookingSerializer
+
